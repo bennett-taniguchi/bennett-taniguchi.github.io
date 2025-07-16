c10eae5bd7080899bc488952a2361d8f3e7f92fd
bennett-taniguchi <tanigb@uw.edu>
2025-07-15 17:03:03 -0700

    S

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
  
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/bennett-taniguchi.github.io",
+  
   plugins: [react(), tailwindcss() ],
   resolve: {
     alias: {
